857132254596b17de8c881f954b5694c7c2c081b
Tom Verran <tom.verran@ovoenergy.com>
2021-02-02 17:57:37 +0000

    show info on hover in a table

--- ui/src/regions.tsx
+++ ui/src/regions.tsx
@@ -3,7 +3,8 @@ import * as React from "react";
 import { Map } from './map'
 
 type State = {
-  data: RegionalData[]
+  data: RegionalData[],
+  hoverRegion?: string
 }
 
 type  DosesByAge = {
@@ -54,7 +55,7 @@ export class Regions extends React.Component<{}, State> {
     if (!this.state || !this.state.data[0]) return 0;
     const percentages = calculatePercentages(this.state.data[0]);
     const max = percentages.reduce((a, b) => Math.max(a, b.percent), 0);
-    return percentages.find(e => id == e.id)?.percent || 1
+    return (percentages.find(e => id == e.id)?.percent || max) / max
   }
 
   updated() {
@@ -62,11 +63,37 @@ export class Regions extends React.Component<{}, State> {
     return formatDistanceToNow(new Date(this.state.data[0].date))
   }
 
+  table(): React.ReactElement {
+    const region = this.state.hoverRegion ? this.state.data[0]?.statistics[this.state.hoverRegion] : null
+    if (region) {
+      return <div className="mt-4 mr-auto">
+        <p className="border-bottom">{region.name}</p>
+        <table className="table w-auto">
+          <tr>
+            <th>Adult population</th>
+            <td className="text-end">{region.population.toLocaleString("en-gb")}</td>
+          </tr>
+          <tr>
+            <th>Over 80s vaccinated</th>
+            <td className="text-end">{region.firstDose.over80.toLocaleString("en-gb")}</td>
+          </tr>
+          <tr>
+            <th>Under 80s vaccinated</th>
+            <td className="text-end">{region.firstDose.under80.toLocaleString("en-gb")}</td>
+          </tr>
+        </table>
+      </div>
+    } else {
+      return <React.Fragment/>
+    }
+  }
+
   render() {
     return <div className="bg-white border shadow-sm p-4">
       <div className="alert alert-info text-center">These data are England only and updated weekly. Last updated {this.updated()} ago.</div>
       <h5 className="mb-4 text-center">Percentage vaccinated by NHS Region</h5>
-      <Map opacity={this.opacity} hover={(region) => console.log(region)}></Map>
+      <Map opacity={this.opacity.bind(this)} hover={(hoverRegion) => this.setState({...this.state, hoverRegion})}></Map>
+      {this.table.call(this)}
       <p className="text-muted mt-4 mb-0">
         The NHS splits its statistics by "Integrated Care Systems" which are essentially administrative regions.
         They replaced Primary Care Trusts in 2013.
